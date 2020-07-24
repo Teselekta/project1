ee9b135afe162b23b094e5781efefa59c0a07dc3
Denis <tardisThome@gmail.com>
2020-07-24 20:52:43 +0300

    created first application

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 "use strict";
 
-let nubmerOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-const moviesNulled = {};
-const actorsNulled = {};
-const genres = [];
-const privat = false;
+const nubmerOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
 
 const personalMovieDB = {
     count: nubmerOfFilms,
-    movies: objNulled,
-    actors: actors,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-}
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
+      
+
